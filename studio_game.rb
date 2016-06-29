da52e70972fbf38e39a2ac2ee207d68dbd8d5ea6
eddieozzie <eddieozzie@gmail.com>
2016-06-28 21:56:05 -0700

    Adds scoring methods

--- studio_game.rb
+++ studio_game.rb
@@ -8,6 +8,17 @@ class Player
 		"I'm #{@name} with a health of #{@health}"
 	end
 
+	def blam()
+		@health -=10
+		puts "#{@name} just got blammed!"
+	end
+
+	def w00t()
+		@health +=15
+		puts "#{@name} just got w00ted!"
+	end
+
+
 end
 
 player1 = Player.new("bob")
@@ -18,4 +29,8 @@ player2 = Player.new("larry", 60)
 puts player2.say_hello
 
 player3 = Player.new("curly", 125)
+player3.blam
+player3.w00t
+
+puts player1
 puts player3.say_hello
