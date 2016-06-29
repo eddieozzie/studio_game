7be50d0a42ef35f0c4b92b32e6f81985da8a073a
eddieozzie <eddieozzie@gmail.com>
2016-06-28 20:52:25 -0700

    Initializes game with player greeting

---
+++ studio_game.rb
@@ -0,0 +1,13 @@
+def get_time()
+	Time.new.strftime("%H:%M:%S")
+
+end
+
+def say_hello(name, health=100)
+	"I'm #{name.capitalize} with a health of #{health} as #{get_time}"
+end
+
+puts say_hello("larry", 60)
+puts say_hello("curly", 125)
+puts say_hello("moe")
+puts say_hello("shemp", 90)
