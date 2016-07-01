301c2384f5a35a0ff125cd5248e39760ec06370a
eddieozzie <eddieozzie@gmail.com>
2016-06-30 17:28:29 -0700

    Adds tests for capitalization, string rep, score, and health to Player test

--- player_spec.rb
+++ player_spec.rb
@@ -7,14 +7,33 @@ describe Player do
 		player.name.should == "Larry"
 	end
 
-	it "has an initial health"
+	it "has an initial health" do
+		player = Player.new("Larry")
+		player.health.should == 100
+	end
 
-	it "has a string representation"
+	it "has a string representation" do
+		player = Player.new("Larry", 10)
+		player.to_s.should == "I'm #{player.name} with a health of #{player.health} and a score of #{player.score}"
+	end
 
-	it "computes a score as the sum of its health and length of name"
+	it "computes a score as the sum of its health and length of name" do
+		player = Player.new("Larry", 10)
+		player.score.should == player.health + player.name.length
+	end
 
-	it "increases health by 15 when w00ted"
+	it "increases health by 15 when w00ted" do
+		health = 10
+		player = Player.new("Larry", health)
+		player.w00t
+		player.health.should == health + 15
+	end
 
-	it "decreases health by 10 when blammed"
+	it "decreases health by 10 when blammed" do
+		health = 10
+		player = Player.new("Larry", health)
+		player.blam
+		player.health.should == health - 10
+	end
 
 end
