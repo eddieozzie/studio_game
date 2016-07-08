de92b59f2af0221856202c0b41e78231a2a74779
eddieozzie <eddieozzie@gmail.com>
2016-07-07 23:55:36 -0700

    Adds berserk player subclass

--- clumsy_player.rb
+++ clumsy_player.rb
@@ -1,14 +1,12 @@
 require_relative 'player'
+require_relative 'treasure_trove'
 
 class ClumsyPlayer < Player
 
 	def found_treasure(treasure)
-    	points = treasure.points / 2.0
-    	@found_treasures[treasure.name] += points
-	    puts "#{@name} found a #{treasure.name} worth #{points} points."
+    	super(Treasure.new(treasure.name, treasure.points/2.0))
   	end
-
-
+  	
 end
 
 if __FILE__ == $0
--- studio_game.rb
+++ studio_game.rb
@@ -1,14 +1,17 @@
 require_relative 'game'
 require_relative 'clumsy_player'
+require_relative 'berserk_player'
 
 player1 = Player.new("bob")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
 player4 = ClumsyPlayer.new("klutz", 105)
+player5 = BerserkPlayer.new("berzerker", 50)
 
 knuckleheads = Game.new("Knuckleheads")
 knuckleheads.load_players(ARGV.shift || "players.csv")
 knuckleheads.add_player(player4)
+knuckleheads.add_player(player5)
 
 loop do 
 	puts "\nHow many rounds would you like to play?"
